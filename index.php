d82506e52db1e7df1751f65c7cbd80afc2f24fe7
Pierre Priot <pierre@couleur-citron.com>
2018-04-05 12:40:45 +0200

    twig for templating

--- index.php
+++ index.php
@@ -4,79 +4,33 @@ $yaml =  'conf.yml';
 /* ------------------------------------------------ */
 	
 $o = (object)yaml_parse_file ($yaml);
-$o->services=(object)($o->services);
-
-var_dump($o);
 
 
-?><!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <meta name="theme-color" content="#3273dc">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
-    <title><?php echo $o->artist.' - '.$o->release; ?></title>
-    <style>
-		a  {
-color: #363636;
+//var_dump($o);
+
+
+$classes = array(
+				'bandcamp' => 'fas fa-bold',
+				'apple' => 'fab fa-apple',
+				'spotify' => 'fab fa-spotify',
+				'deezer' => 'fas fa-signal',
+);
+
+foreach($o->services as $k => $aVal){
+	if (isset($classes[strtolower($aVal['name'])])){		
+		$o->services[$k]['class']=$classes[strtolower($aVal['name'])];		
+	}	
 }
-		
-	</style>
-</head>
-<body>
-<section class="hero is-info is-bold">
-    <div class="hero-body">
-        <div class="container">
-            <h1 class="title"><?php echo $o->artist.' - '.$o->release; ?></h1>
-        </div>
-    </div>
-</section>
-<section class="section">
-    <div class="container">
-        <div class="columns">
-            <div class="column">
-                <div class="panel">
-                    
-                    <div class="panel-block">
-                        <div class="content">
-                            <p>
-                               <img src="<?php echo $o->img; ?>"  alt="<?php echo $o->artist.' - '.$o->release; ?>">                               
-                            </p>
-                           
-                        </div>
-                    </div>
 
-                </div>
-            </div>
-            <div class="column">
-               
-               <div class="panel">
-                    <p class="panel-heading">
-                        <span class="icon"><i class="fas fa-bold"></i></span>
-                        Bandcamp
-                    </p>                    
-                </div>
-                
-               <div class="panel">
-                    <p class="panel-heading">
-                        <span class="icon"><i class="fab fa-apple"></i></span>
-                        iTunes - Apple Music
-                    </p>                   
-                </div>
-                
-                 <div class="panel">
-                        <p class="panel-heading">
-                        <span class="icon"><i class="fab fa-spotify"></i></span>
-                        Spotify
-                    </p>
-                </div>
-                
-                <div class="panel">
-                    <p class="panel-heading">
-                        <span class="icon"><i class="fas fa-signal"></i></span>
-							<a href="<?php echo $o->services->Deezer; ?>" class="is-info">Deezer</a>
-                    </p>
-                </div>
-       
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('twig');
+$twig = new Twig_Environment($loader, array(
+    //'cache' => 'cache',
+	'cache' => false
+));
+
+$template = $twig->load('index.html');
+echo $template->render(array('artist' => $o->artist, 'release' => $o->release, 'img' => $o->img, 'services' => $o->services));
+
+?>
