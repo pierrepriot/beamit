701e807f001b1ae837c210a8e4540e35fa526575
Pierre Priot <pierre@couleur-citron.com>
2018-04-09 17:01:45 +0200

    object oriented rewrite

--- index.php
+++ index.php
@@ -2,54 +2,67 @@
 $yaml =  'conf.yml';
 
 /* ------------------------------------------------ */
+
+class BeamIt
+{
+    private $o;
+	private $classes = array(
+						'bandcamp' => 'fas fa-bold',
+						'apple' => 'fab fa-apple',
+						'spotify' => 'fab fa-spotify',
+						'deezer' => 'fas fa-signal',
+						'youtube' => 'fab fa-youtube',
+						'facebook' => 'fab fa-facebook',
+						'twitter' => 'fab fa-twitter-square',
+						'instagram' => 'fab fa-instagram',
+		);
+
+	public function __construct($yaml) {
+		$this->conf = (object)yaml_parse_file ($yaml);
+		
+		foreach($this->conf->services as $k => $aVal){
+			if (isset($this->classes[strtolower($aVal['name'])])){		
+				$this->conf->services[$k]['class']=$this->classes[strtolower($aVal['name'])];		
+			}	
+		}
+
+		foreach($this->conf->socials as $k => $aVal){
+			if (isset($this->classes[strtolower($aVal['name'])])){		
+				$this->conf->socials[$k]['class']=$this->classes[strtolower($aVal['name'])];		
+			}	
+		}
+
+		if ( $_SERVER['HTTPS'] != null){
+			$this->conf->baseurl = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		}
+		else{
+			$this->conf->baseurl = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		}
+	 }
 	
-$o = (object)yaml_parse_file ($yaml);
-
-
-//var_dump($o);
-
-
-$classes = array(
-				'bandcamp' => 'fas fa-bold',
-				'apple' => 'fab fa-apple',
-				'spotify' => 'fab fa-spotify',
-				'deezer' => 'fas fa-signal',
-				'youtube' => 'fab fa-youtube',
-				'facebook' => 'fab fa-facebook',
-				'twitter' => 'fab fa-twitter-square',
-				'instagram' => 'fab fa-instagram',
-);
-
-foreach($o->services as $k => $aVal){
-	if (isset($classes[strtolower($aVal['name'])])){		
-		$o->services[$k]['class']=$classes[strtolower($aVal['name'])];		
-	}	
+	
+	public function getConf(){
+		
+		return array(
+			'ogurl' => $_SERVER['REQUEST_URI'], 
+			'artist' => $this->conf->artist, 
+			'release' => $this->conf->release, 
+			'img' => $this->conf->img, 
+			'services' => $this->conf->services, 
+			'socials' => $this->conf->socials, 
+			'twitternic' => $this->conf->twitternic, 
+			'baseurl' => $this->conf->baseurl);		
+	}
 }
-
-foreach($o->socials as $k => $aVal){
-	if (isset($classes[strtolower($aVal['name'])])){		
-		$o->socials[$k]['class']=$classes[strtolower($aVal['name'])];		
-	}	
-}
-
-if ( $_SERVER['HTTPS'] != null){
-	$o->baseurl = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-}
-else{
-	$o->baseurl = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-}
+	
+$o = new BeamIt($yaml);
 
 require_once 'vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem('twig');
-
-
 $twig = new Twig_Environment($loader, array(
     //'cache' => 'cache',
 	'cache' => false
 ));
-
 $template = $twig->load('index.html');
-echo $template->render(array('ogurl' => $_SERVER['REQUEST_URI'], 'artist' => $o->artist, 'release' => $o->release, 'img' => $o->img, 'services' => $o->services, 'socials' => $o->socials, 'twitternic' => $o->twitternic, 'baseurl' => $o->baseurl));
-
-?>
+echo $template->render($o->getConf());
