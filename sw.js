265ba3b5f4cff52679983733c7a2e5e287686862
Pierre Priot <pierre@couleur-citron.com>
2018-04-17 16:47:37 +0200

    workBox as SW

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
 console.log('Hello from sw.js');
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
